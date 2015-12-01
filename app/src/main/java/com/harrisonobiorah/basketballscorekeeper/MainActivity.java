b0cd9ee96e30b65fbc038f234b22ef85948ff46f
Harrison Obiorah <harrison.obiorah@gmail.com>
2015-12-01 10:48:21 -0500

    Styled the UI and fixed  scoring mechanism

--- app/src/main/java/com/harrisonobiorah/basketballscorekeeper/MainActivity.java
+++ app/src/main/java/com/harrisonobiorah/basketballscorekeeper/MainActivity.java
@@ -4,36 +4,92 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
 
 
-public class MainActivity extends ActionBarActivity {
-
+    private int homeScore = 0;
+    private int awayScore = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //this method is how the application knows what view to pull up the screen. The ui is defined in the layout folder
+        //so we are able to access the layout file in
         setContentView(R.layout.activity_main);
+        updateScore();
+
+    }
+
+    public void plusOneH(View view)
+    {
+        homeScore++;
+        updateScore();
+        gameOver();
+    }
+
+    public void plusTwoH(View view)
+    {
+        homeScore += 2;
+        updateScore();
+        gameOver();
+    }
+
+    public void plusOneA(View view)
+    {
+        awayScore++;
+        updateScore();
+        gameOver();
+    }
+
+    public void plusTwoA(View view)
+    {
+        awayScore += 2;
+
+        updateScore();
+        gameOver();
+    }
+
+    public void gameOver()
+    {
+       if(homeScore>=11)
+       {
+           TextView homeTeam = (TextView) findViewById(R.id.winningTeam);
+           homeTeam.setText("Home Team Won!");
+       }
+        else if(awayScore>=11)
+       {
+           TextView awayTeam = (TextView) findViewById(R.id.winningTeam);
+           awayTeam.setText("Away Team Won!");
+       }
+
+
+    }
+
+    public void updateScore()
+    {
+        TextView homeTeam = (TextView) findViewById(R.id.homePoints_text_view);
+        TextView awayTeam = (TextView) findViewById(R.id.awayPoints_text_view);
+
+        homeTeam.setText(" " +homeScore);
+        awayTeam.setText(" " +awayScore);
+
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void reset(View view)
+    {
+        homeScore =0;
+        awayScore = 0;
+        updateScore();
+        TextView team = (TextView) findViewById(R.id.winningTeam);
+        team.setText("");
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
-        return super.onOptionsItemSelected(item);
-    }
 }
